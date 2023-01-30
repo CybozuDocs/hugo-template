08b5bf1c7367270939954de7c5c8bc1988ac4951
piyo <chick.p.1120@gmail.com>
2023-01-30 13:32:50 +0900

    Fix collect path

--- static/javascripts/geolocation.js
+++ static/javascripts/geolocation.js
@@ -20,7 +20,7 @@
 
   async function getGeolocation() {
     try {
-      const resp = await fetch("https://resplendent-gecko-62f981.netlify.app/geolocation");
+      const resp = await fetch("/geolocation");
       const json = await resp.json();
       return json;
     } catch (err) {
