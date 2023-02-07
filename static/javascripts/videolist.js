808546151c812dc6566be13c25527ab92f6dc070
h-fukuda <h-fukuda@cybozu.co.jp>
2023-02-07 17:03:22 +0900

    supported anchor link

--- static/javascripts/videolist.js
+++ static/javascripts/videolist.js
@@ -27,6 +27,19 @@
                         this.filter = response.data.title;
                 })
                 .catch((error) => console.log(error));
+
+            // アンカー付きURL対応
+            // コンマ5秒待ってから移動させる
+            this.$nextTick(function () {
+                const hash = window.location.hash;
+                if(hash !== undefined) {
+                    const hashBody = hash.replace("#", "");
+                    setTimeout(() => {
+                        const el = document.getElementById(hashBody);
+                        el.scrollIntoView();
+                    }, 500);
+                }
+            })
         },
         methods: {
             // タグ一覧の選択状態の変更
