4769e1e479e4a965a5d709a2d4757a4638ce2e1e
piyo <chick.p.1120@gmail.com>
2023-07-03 14:18:21 +0900

    Implement home menus

---
+++ static/javascripts/gr6home.js
@@ -0,0 +1,85 @@
+'use strict';
+
+(function() {
+
+    // Menu component
+    const MenuCard = {
+        props: ["type", "item", "admin"],
+        template: `<div>
+                        <a v-bind:href="this.item.link_href" target="_self" class="home-menu-item-title">
+                            <div v-bind:class="'home-menu-item-title-img home-menu-item-title-img-' + this.type" v-bind:style="{ backgroundImage: 'url(' + this.item.img_src + ')' }"></div>
+                            <div class="home-menu-item-text">
+                                    {{ this.item.title }}
+                            </div>
+                        </a>
+                        <div class="home-menu-item-description">{{ this.item.description }}</div>
+                    </div>
+                    <div v-if="this.item.admin_href" class="home-menu-item-admin" >
+                        <div class="home-menu-item-admin-img" v-bind:style="{ backgroundImage: 'url(' + this.admin.icon + ')' }"></div>
+                        <div class="home-menu-item-admin-text" >
+                            <a v-bind:href="this.admin.link_href">{{ this.admin.title }}</a>
+                        </div>
+                    </div>`
+    };
+    const vm = Vue.createApp({
+        delimiters: ['[[', ']]'],
+        data() {
+            return {
+                menus: [],
+                langid: "en",
+                productid: "",
+            }
+        },
+        created() {
+            this.langid = getLangId();
+            this.productid = getProductId();
+        },
+        components: {
+            MenuCard
+        },
+        async mounted() {
+            fetch(`/${this.productid}/${this.langid}/home.json`, { cache: 'no-cache' }).then((resp) => {
+                if(!resp.ok) {
+                    throw new Error(resp.statusText);
+                }
+                return resp.json();
+            }).then((json) => {
+                this.menus = json;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        methods: {
+            show: function() {
+            }
+        }
+    }).mount('#content');
+
+
+    // URLから対象言語を取り出す
+    function getLangId() {
+        let langid = "en";
+        const url = new URL(window.location.href);
+        const pname = url.pathname;
+        const pathes = pname.split("/");
+        if (pathes.length > 2) {
+            langid = pathes[2].replace(".html", "");
+        }
+        return langid;
+    }
+
+    // URLから製品IDを取り出す
+    function getProductId() {
+        let prodid = "en";
+        const url = new URL(window.location.href);
+        const pname = url.pathname;
+        const pathes = pname.split("/");
+        if(pathes.length > 2) {
+            prodid = pathes[1];
+        }
+
+        return prodid;
+    }
+
+    vm.show();
+})();
