75df6ef8a36dd1a3927fdcd824a28ab7df82effb
h-fukuda <h-fukuda@cybozu.co.jp>
2025-01-06 13:04:11 +0900

    fixed the path for id link on WOVN environment

--- static/javascripts/application.js
+++ static/javascripts/application.js
@@ -1110,12 +1110,14 @@
                     const url = location.href;
                     const pathes = url.split("/");
                     
-                    let prodpath = "";
+                    let langPath = pathes[2];
+                    if (window.WOVN && WOVN.io && WOVN.io.isApiReady()) {
+                        langPath = WOVN.io.getCurrentLang().code;
+                    }
+                    let prodpath = "//" + langPath;
                     if (languages.indexOf(pathes[3]) === -1) {
-                        // URLの２階層目が言語情報ではない（＝製品ID）
-                        prodpath = "//" + pathes[2] + "/" + pathes[3];
-                    } else {
-                        prodpath = "//" + pathes[2];
+                        // URLの4階層目が言語情報ではない（＝製品ID）
+                        prodpath = prodpath + "/" + pathes[3];
                     }
                     
                     let desturl = pathes[0] + prodpath + idpath + ".html";
