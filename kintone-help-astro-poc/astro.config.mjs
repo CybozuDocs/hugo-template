ee70daf2ac515757c7f8ce1554fcba205324b971
mugi-uno <mugi.uno@gmail.com>
2025-06-11 17:22:53 +0900

    update

--- kintone-help-astro-poc/astro.config.mjs
+++ kintone-help-astro-poc/astro.config.mjs
@@ -1,7 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import dotenv from "dotenv";
 import mdx from "@astrojs/mdx";
+
+const envMap = new Map();
+envMap.set("jp", ".env.jp");
+envMap.set("us", ".env.us");
+envMap.set("cn", ".env.cn");
+envMap.set("jp_staging", ".env.jp_staging");
+envMap.set("us_staging", ".env.us_staging");
+envMap.set("cn_staging", ".env.cn_staging");
+envMap.set("dev", ".env");
+
+dotenv.config({
+  path: envMap.get(process.env.ENV) || envMap.get("dev"),
+});
 
 // https://astro.build/config
 export default defineConfig({
