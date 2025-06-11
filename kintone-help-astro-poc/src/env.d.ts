91e287ecc2e64b01722660cfa3c429d8acdc05be
mugi-uno <mugi.uno@gmail.com>
2025-06-11 13:31:33 +0900

    環境変数を見直し

--- kintone-help-astro-poc/src/env.d.ts
+++ kintone-help-astro-poc/src/env.d.ts
@@ -6,21 +6,21 @@ interface ImportMetaEnv {
   readonly PUBLIC_TEMPLATE_VERSION: string;
   readonly PUBLIC_PRODUCT: string;
   readonly PUBLIC_DOMAIN: string;
-  readonly PUBLIC_TARGET_REGION: string;
+  readonly PUBLIC_TARGET_REGION: "JP" | "US" | "CN";
   readonly PUBLIC_KINTONE: string;
   readonly PUBLIC_SERVICE: string;
   readonly PUBLIC_CYBOZU_COM: string;
   readonly PUBLIC_FAVICON: string;
   readonly PUBLIC_LOGO: string;
   readonly PUBLIC_OG_IMG: string;
-  
+
   // 機能フラグ
   readonly PUBLIC_LANG_SELECTOR: string;
   readonly PUBLIC_MEGANAV: string;
   readonly PUBLIC_JSON_TREE: string;
   readonly PUBLIC_GOOGLE_SEARCH: string;
   readonly PUBLIC_ID_SEARCH: string;
-  
+
   // 外部サービス
   readonly PUBLIC_DEVNET_NAME: string;
   readonly PUBLIC_DEVNET_URL: string;
@@ -31,17 +31,17 @@ interface ImportMetaEnv {
   readonly PUBLIC_CHAT_MENU_PREFIX?: string;
   readonly PUBLIC_SERVICE_TYPE?: string;
   readonly PUBLIC_SERVICE_TYPE_ID?: string;
-  
+
   // カスタムCSS
   readonly PUBLIC_CUSTOM_CSS: string;
-  
+
   // ラベル色設定
   readonly PUBLIC_LABEL_COLORS: string;
-  
+
   // 言語設定
   readonly PUBLIC_DEFAULT_CONTENT_LANGUAGE: string;
   readonly PUBLIC_LANGUAGE_CODE: string;
-  
+
   // 日本語設定
   readonly PUBLIC_PRODUCT_NAME_JA: string;
   readonly PUBLIC_HELP_JA: string;
@@ -55,7 +55,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_SLASH_UI_JA: string;
   readonly PUBLIC_LABEL_LEAD_JA: string;
   readonly PUBLIC_LABEL_CONTENTS_JA: string;
-  
+
   // 英語設定
   readonly PUBLIC_PRODUCT_NAME_EN: string;
   readonly PUBLIC_HELP_EN: string;
@@ -67,7 +67,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_SLASH_HELP_EN: string;
   readonly PUBLIC_SLASH_ADMINISTRATORS_EN: string;
   readonly PUBLIC_SLASH_UI_EN: string;
-  
+
   // 中国語（簡体字）設定
   readonly PUBLIC_PRODUCT_NAME_ZH: string;
   readonly PUBLIC_HELP_ZH: string;
@@ -79,7 +79,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_SLASH_HELP_ZH: string;
   readonly PUBLIC_SLASH_ADMINISTRATORS_ZH: string;
   readonly PUBLIC_SLASH_UI_ZH: string;
-  
+
   // 中国語（繁体字）設定
   readonly PUBLIC_PRODUCT_NAME_ZH_TW: string;
   readonly PUBLIC_HELP_ZH_TW: string;
@@ -91,7 +91,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_SLASH_HELP_ZH_TW: string;
   readonly PUBLIC_SLASH_ADMINISTRATORS_ZH_TW: string;
   readonly PUBLIC_SLASH_UI_ZH_TW: string;
-  
+
   // 動的に生成される言語固有の設定用
   [key: string]: string | undefined;
 }
